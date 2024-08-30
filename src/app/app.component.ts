8abfb11bdedfdd0acf0315ca6e6edd888be93e15
balamurugan.r <balamurugan.r@raster.in>
2024-08-30 13:35:38 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+const ONE_DAY = 24 * 60 * 60 * 1000;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'], 
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'datetime';
+  
+  public min: Date = new Date(Date.now() - 30 * ONE_DAY);
+  public max: Date = new Date();
+  protected selectedDates: [Date, Date] = [
+    new Date(Date.now() - ONE_DAY),
+    new Date(Date.now() + ONE_DAY)
+  ];
+
+  protected currentValue: Date = new Date(this.selectedDates[0]);
+
+  protected endValue: Date = new Date(this.selectedDates[1]);
+
+  public myFilter = (date: Date): boolean =>  { 
+    const day:number = date.getDay();
+    return day !== 0 && day !== 6;
+  }
+
+  protected selectedTrigger(date: Date): void {
+    console.log(date);
+  }
+
 }
+ 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RIDateTimeModule, RINativeDateTimeModule } from "ri-datetime-picker";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule, 
+        RIDateTimeModule, 
+        RINativeDateTimeModule, 
+        
+    ],
+    exports: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
